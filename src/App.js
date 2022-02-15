57e44db35865187116e95601adbba065c74f0360
halikali <tunchalikali@gmail.com>
2022-02-15 23:40:50 +0300

    import the navbar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 
+import NavgationBar from "./components/NavgationBar";
 import routes from "./routes";
 import "./app.scss";
 
 const App = () => {
   return (
     <div className="app">
+      <NavgationBar />
       <Routes>
         {routes.map((route) => (
           <Route path={route.path} element={route.page} key={route.name} />
