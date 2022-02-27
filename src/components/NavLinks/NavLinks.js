b52354643cd2a50c0de5442c8de594213689af05
halikali <tunchalikali@gmail.com>
2022-02-27 20:30:09 +0300

    active class is attribute for NavLink now

--- src/components/NavLinks/NavLinks.js
+++ src/components/NavLinks/NavLinks.js
@@ -12,33 +12,57 @@ const NavLinks = ({ children }) => {
             Home
           </NavLink>
         </li>
-        <li className="navbar__list-item" activeClassName="active">
-          <NavLink to={"/shop"} className="navbar__link">
+        <li className="navbar__list-item">
+          <NavLink
+            to={"/shop"}
+            className="navbar__link"
+            activeClassName="active"
+          >
             Shop
           </NavLink>
         </li>
-        <li className="navbar__list-item" activeClassName="active">
-          <NavLink to={"/features"} className="navbar__link">
+        <li className="navbar__list-item">
+          <NavLink
+            to={"/features"}
+            className="navbar__link"
+            activeClassName="active"
+          >
             Features
           </NavLink>
         </li>
-        <li className="navbar__list-item" activeClassName="active">
-          <NavLink to={"/lookbook"} className="navbar__link">
+        <li className="navbar__list-item">
+          <NavLink
+            to={"/lookbook"}
+            className="navbar__link"
+            activeClassName="active"
+          >
             Lookbook
           </NavLink>
         </li>
-        <li className="navbar__list-item" activeClassName="active">
-          <NavLink to={"/pages"} className="navbar__link">
+        <li className="navbar__list-item">
+          <NavLink
+            to={"/pages"}
+            className="navbar__link"
+            activeClassName="active"
+          >
             pages
           </NavLink>
         </li>
-        <li className="navbar__list-item" activeClassName="active">
-          <NavLink to={"/blog"} className="navbar__link">
+        <li className="navbar__list-item">
+          <NavLink
+            to={"/blog"}
+            className="navbar__link"
+            activeClassName="active"
+          >
             Blog
           </NavLink>
         </li>
-        <li className="navbar__list-item" activeClassName="active">
-          <NavLink to={"/buy"} className="navbar__link">
+        <li className="navbar__list-item">
+          <NavLink
+            to={"/buy"}
+            className="navbar__link"
+            activeClassName="active"
+          >
             Buy now
           </NavLink>
         </li>
