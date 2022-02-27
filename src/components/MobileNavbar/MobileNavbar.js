7d92cd527e07f5b0a15bb9c8ac878fb244fb0ef2
halikali <tunchalikali@gmail.com>
2022-02-27 03:23:05 +0300

    add extra links to nav links for mobile

--- src/components/MobileNavbar/MobileNavbar.js
+++ src/components/MobileNavbar/MobileNavbar.js
@@ -1,11 +1,43 @@
 import React from "react";
 import { createPortal } from "react-dom";
+import { NavLink } from "react-router-dom";
 
+import NavLinks from "../NavLinks";
 import "./MobileNavbar.scss";
 
-const MobileNavbar = () => {
+const MobileNavbar = ({ isOpen }) => {
   return createPortal(
-    <div className="mobile-navbar">MobileNavbar</div>,
+    <div className={`mobile-navbar ${isOpen ? "opened" : "closed"}`}>
+      <NavLinks>
+        <li className="navbar__list-item" activeClassName="active">
+          <NavLink
+            to={"/login"}
+            className="navbar__link"
+            activeClassName="active"
+          >
+            Login
+          </NavLink>
+        </li>
+        <li className="navbar__list-item" activeClassName="active">
+          <NavLink
+            to={"/register"}
+            className="navbar__link"
+            activeClassName="active"
+          >
+            Regıster
+          </NavLink>
+        </li>
+        <li className="navbar__list-item" activeClassName="active">
+          <NavLink
+            to={"/whislist"}
+            className="navbar__link"
+            activeClassName="active"
+          >
+            Whıslıst
+          </NavLink>
+        </li>
+      </NavLinks>
+    </div>,
     document.getElementById("mobile-navbar")
   );
 };
