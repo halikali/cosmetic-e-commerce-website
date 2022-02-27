b0ab8a1e8545edf7b456fb2ecb063307a0c873ab
halikali <tunchalikali@gmail.com>
2022-02-27 23:18:07 +0300

    complete the structure of carousel

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,37 +1,57 @@
 import React from "react";
-import { Carousel } from "react-bootstrap";
+import { Button, Carousel } from "react-bootstrap";
 
 import "./Slider.scss";
 
 const Slider = () => {
   return (
-    <div>
-      <Carousel fade>
-        <Carousel.Item>
+    <Carousel fade className="carousel">
+      <Carousel.Item className="carousel__item">
+        <picture>
+          <source
+            media="(min-width:768px)"
+            srcset="https://cdn.shopify.com/s/files/1/0036/7306/3491/files/dome1-bnr2.jpg?v=1609224938"
+          />
           <img
             className="d-block w-100"
             src="https://cdn.shopify.com/s/files/1/0036/7306/3491/files/default-mb-bnr2.jpg?v=1609224882"
             alt="First slide"
           />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
+        </picture>
+
+        <Carousel.Caption className="carousel__caption carousel__caption--right ">
+          <b className="carousel__caption--title">
+            Speed up your store to convert more Sales
+          </b>
+          <span className="carousel__caption--text">
+            The Powerful Theme You can Trust
+          </span>
+          <Button variant="dark">Shop Now</Button>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item className="carousel__item">
+        <picture>
+          <source
+            media="(min-width:768px)"
+            srcset="https://cdn.shopify.com/s/files/1/0036/7306/3491/files/dome1-bnr1.jpg?v=1609224938"
+          />
           <img
             className="d-block w-100"
             src="https://cdn.shopify.com/s/files/1/0036/7306/3491/files/default-mb-bnr1.jpg?v=1609224882"
             alt="Second slide"
           />
-
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-    </div>
+        </picture>
+        <Carousel.Caption className="carousel__caption carousel__caption--left ">
+          <b className="carousel__caption--title">
+            Speed up your store to convert more Sales
+          </b>
+          <span className="carousel__caption--text">
+            The Powerful Theme You can Trust
+          </span>
+          <Button variant="dark">Shop Now</Button>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
   );
 };
 
