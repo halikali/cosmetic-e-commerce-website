f41269620319fbc2d744e1207a07e6943967b328
halikali <tunchalikali@gmail.com>
2022-02-27 23:19:56 +0300

    import slider to home page

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,31 +1,18 @@
 import React from "react";
-import { Button, Col, Row } from "react-bootstrap";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCoffee } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
+import { Col, Row } from "react-bootstrap";
 
+import Slider from "../../components/Slider";
 import "./HomePage.scss";
 
 const HomePage = () => {
   return (
-    <div>
-      <FontAwesomeIcon icon={faCoffee} />
-      <Row className="mx-0">
-        <Button as={Col} variant="primary">
-          <Link to="/" className="text-white">
-            Anasayfa
-          </Link>
-        </Button>
-        <Button as={Col} variant="secondary" className="mx-2">
-          <Link to={"/secondpage"} className="text-white">
-            İkinci sayfa
-          </Link>
-        </Button>
-        <Button as={Col} variant="success">
-          Button #3
-        </Button>
+    <>
+      <Row>
+        <Col>
+          <Slider />
+        </Col>
       </Row>
-    </div>
+    </>
   );
 };
 
