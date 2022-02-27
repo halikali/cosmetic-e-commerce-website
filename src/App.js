df30498581c11fcecb9e1fb5f727cd9105907eb5
halikali <tunchalikali@gmail.com>
2022-02-27 20:28:04 +0300

    fix the component name

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 
-import NavgationBar from "./components/NavgationBar";
+import NavigationBar from "./components/NavigationBar";
 import routes from "./routes";
 import "./app.scss";
 
 const App = () => {
   return (
     <div className="app">
-      <NavgationBar />
+      <NavigationBar />
       <Routes>
         {routes.map((route) => (
           <Route path={route.path} element={route.page} key={route.name} />
