b83e8ba229a2edfa34d9b89eeb0111a1cd806714
safakbirkan <150301403@student.beykent.edu.tr>
2022-03-07 16:40:40 +0300

    footer-branch / Footer's styling completed.

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -9,32 +9,32 @@ export class Footer extends Component {
       <footer className='footer'>
         <div className='footer__container'>
             <div className='footer__col'>
-                <div className='footer__logo'><a href='#'>LOGO</a></div>
+                <div className='footer__logo'><strong>AVONE</strong></div>
                 <div className='footer__item'>55 Gallaxy Enque, <br></br> 2568 steet, 23568 NY</div>
                 <div className='footer__item'><strong>Phone:</strong> (444) 00 00 </div>
                 <div className='footer__item'><strong>Email:</strong> info@cosmetic.com </div>
             </div>
             <div className='footer__col'>
                 <div className='footer__title'><strong>INFORMATION</strong></div>
-                <div className='footer__item'><a href='#'>About us</a></div>
-                <div className='footer__item'><a href='#'>Contact Us</a></div>
-                <div className='footer__item'><a href='#'>Career</a></div>
-                <div className='footer__item'><a href='#'>My Account</a></div>
-                <div className='footer__item'><a href='#'>Orders and Returns</a></div>
+                <div className='footer__item'><a href='#' className='footer__link'>About us</a></div>
+                <div className='footer__item'><a href='#' className='footer__link'>Contact Us</a></div>
+                <div className='footer__item'><a href='#' className='footer__link'>Career</a></div>
+                <div className='footer__item'><a href='#' className='footer__link'>My Account</a></div>
+                <div className='footer__item'><a href='#' className='footer__link'>Orders and Returns</a></div>
             </div>
             <div className='footer__col'>
                 <div className='footer__title'><strong>CATEGORIES</strong></div>
-                <div className='footer__item'><a href='#'>Makeup</a></div>
-                <div className='footer__item'><a href='#'>Hair</a></div>
-                <div className='footer__item'><a href='#'>Body</a></div>
-                <div className='footer__item'><a href='#'>Perfumes</a></div>
-                <div className='footer__item'><a href='#'>Fashion</a></div>
+                <div className='footer__item'><a href='#' className='footer__link'>Makeup</a></div>
+                <div className='footer__item'><a href='#' className='footer__link'>Hair</a></div>
+                <div className='footer__item'><a href='#' className='footer__link'>Body</a></div>
+                <div className='footer__item'><a href='#' className='footer__link'>Perfumes</a></div>
+                <div className='footer__item'><a href='#' className='footer__link'>Fashion</a></div>
             </div>
             <div className='footer__col'>
-                <div className='footer__logo'><strong>SOCIAL MEDIA</strong></div>
-                <div className='footer__item'><a href='#'><FontAwesomeIcon icon="fa-brands fa-facebook-f" /></a></div>
-                <div className='footer__item'><a href='#'><FontAwesomeIcon icon="fa-brands fa-twitter" /></a></div>
-                <div className='footer__item'><a href='#'><FontAwesomeIcon icon="fa-brands fa-instagram" /></a></div>
+                <div className='footer__title'><strong>SOCIAL MEDIA</strong></div>
+                <div className='footer__item'><a href='#' className='footer__link'><FontAwesomeIcon icon="fa-brands fa-facebook-f" /></a></div>
+                <div className='footer__item'><a href='#' className='footer__link'><FontAwesomeIcon icon="fa-brands fa-twitter" /></a></div>
+                <div className='footer__item'><a href='#' className='footer__link'><FontAwesomeIcon icon="fa-brands fa-instagram" /></a></div>
                 <FontAwesomeIcon icon={'facebook'} />
             </div>
         </div>
