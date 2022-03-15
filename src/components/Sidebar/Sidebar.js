b44de7ed5382a8343f18897dde79d806eb60628b
halikali <tunchalikali@gmail.com>
2022-03-15 11:49:58 +0300

    complete the sidebar component

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,7 +1,81 @@
+import Checkbox from "./Checkbox";
 import "./Sidebar.scss";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <div className="categories">
+        <h4 className="sidebar__title">Categories</h4>
+        <ul
+          className="categoris__list-wrapper"
+          style={{ padding: 0, margin: 0 }}
+        >
+          <li className="categories__list-item">Women</li>
+          <li className="categories__list-item">Men</li>
+          <li className="categories__list-item">Shoes</li>
+          <li className="categories__list-item">Popular</li>
+          <li className="categories__list-item">Best Seller</li>
+          <li className="categories__list-item">Accessories</li>
+        </ul>
+      </div>
+      <div className="size">
+        <h4 className="sidebar__title">Size</h4>
+        <div className="size__list-wrapper">
+          {[
+            { size: "xs" },
+            { size: "s" },
+            { size: "m" },
+            { size: "l" },
+            { size: "xl" },
+          ].map((item) => (
+            <Checkbox name={item.size} />
+          ))}
+        </div>
+      </div>
+      <div className="product-type">
+        <h4 className="sidebar__title">PRODUCT TYPE</h4>
+        <div className="product-type__list-wrapper">
+          {[
+            { type: " Accessories " },
+            { type: " Dress " },
+            { type: " Shirts " },
+            { type: " Shoes " },
+            { type: " Shorts " },
+            { type: " Tshirts " },
+            { type: " Women's shoes " },
+          ].map((item) => (
+            <Checkbox name={item.type} />
+          ))}
+        </div>
+      </div>
+      <div className="brand">
+        <h4 className="sidebar__title">BRAND</h4>
+        <div className="brand__list-wrapper">
+          {[
+            { brandName: "Amyar" },
+            { brandName: "Anne" },
+            { brandName: "brunglia" },
+            { brandName: "burberry" },
+            { brandName: "Camper" },
+            { brandName: "Charcoal" },
+            { brandName: "Croock" },
+            { brandName: "Givenchy" },
+            { brandName: "Womeni" },
+          ].map((item) => (
+            <Checkbox name={item.brandName} />
+          ))}
+        </div>
+      </div>
+      <div className="availability">
+        <h4 className="sidebar__title">AVAILABILITY</h4>
+        <div className="availability__list-wrapper">
+          {[{ status: "In stock" }, { status: "Out of stock" }].map((item) => (
+            <Checkbox name={item.status} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
