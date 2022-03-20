1140d532f82c0895301f3acc33b70be55cc2bb78
halikali <tunchalikali@gmail.com>
2022-03-20 21:24:15 +0300

    wrap navbar with container

--- src/App.js
+++ src/App.js
@@ -2,20 +2,22 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 import NavigationBar from "./components/NavigationBar";
-import {Footer} from './components/Footer/footer';
+import { Footer } from "./components/Footer/footer";
 import routes from "./routes";
 import "./app.scss";
 
 const App = () => {
   return (
     <div className="app">
-      <NavigationBar />
+      <div className="container">
+        <NavigationBar />
+      </div>
       <Routes>
         {routes.map((route) => (
           <Route path={route.path} element={route.page} key={route.name} />
         ))}
       </Routes>
-      <Footer/>
+      <Footer />
     </div>
   );
 };
