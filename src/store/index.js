9d8def38e3c967a04a7baad90265eab81c9b37a0
halikali <tunchalikali@gmail.com>
2022-05-25 17:09:51 +0300

    add new reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
+import productDetailsReducer from "./reducers/getProductDetails";
 
 import productReducer from "./reducers/productReducer";
 
 const rootReducer = combineReducers({
   product: productReducer,
+  details: productDetailsReducer,
 });
 
 export default rootReducer;
