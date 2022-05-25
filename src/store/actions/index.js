939c2735a9ed48318750c41fa805d77fe639127e
halikali <tunchalikali@gmail.com>
2022-05-25 17:09:35 +0300

    define action

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,7 +1,19 @@
 export const getAllProducts = () => {
   return (dispatch) => {
-    dispatch({ type: "GET_ALL_PRODUCTS_REQUEST" });
+    fetch("http://localhost:4000/products/")
+      .then((response) => response.json())
+      .then((products) =>
+        dispatch({ type: "GET_ALL_PRODUCTS_SUCCESS", payload: products })
+      );
+  };
+};
 
-    dispatch({ type: "GET_ALL_PRODUCTS_SUCCESS" });
+export const getProductById = (id) => {
+  return (dispatch) => {
+    fetch(`http://localhost:4000/products/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        dispatch({ type: "GET_PRODUCT_DETAILS_SUCCESS", payload: data });
+      });
   };
 };
