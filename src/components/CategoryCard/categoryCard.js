c46564a770556a4d62e0395859f06d9108ea11ee
halikali <tunchalikali@gmail.com>
2022-05-25 17:08:04 +0300

    dynamization of the category card component

--- src/components/CategoryCard/categoryCard.js
+++ src/components/CategoryCard/categoryCard.js
@@ -1,33 +1,38 @@
-import React, {Component} from 'react';
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faCartShopping,
   faMagnifyingGlass,
-  faHeart
+  faHeart,
 } from "@fortawesome/free-solid-svg-icons";
 
-import './categoryCard.scss';
+import "./categoryCard.scss";
 
-export class CategoryCard extends Component {
-  render() {
-    return (
-      <div className='category-card'>
-        <img className='category-card__image' src="https://cdn.shopify.com/s/files/1/0036/7306/3491/products/8_d74500ed-dd16-4711-b826-3c25f5651c8a_350x.jpg?v=1616747573" />
-        <div className='category-card__icons'>
+export const CategoryCard = ({ productName, coverImage, price, colors }) => {
+  return (
+    <div className="category-card">
+      <img className="category-card__image" src={coverImage && coverImage} />
+      <div className="category-card__icons">
         <FontAwesomeIcon icon={faHeart} className={"card-icon"} />
         <FontAwesomeIcon icon={faCartShopping} className={"card-icon"} />
         <FontAwesomeIcon icon={faMagnifyingGlass} className={"card-icon"} />
-        </div>
-        <div className='category-card__figcaption'>
-          <div className='category-card__title'>Button Up Top Black</div>
-          <div className='category-card__price'>500 $</div>
-        </div>
-        <ul className='category-card__colors'>
-          <li className='category-card__color green'></li>
-          <li className='category-card__color pink'></li>  
-          <li className='category-card__color gray'></li>  
-        </ul>
       </div>
-    );
-  }
-}
+      <div className="category-card__figcaption">
+        <div className="category-card__title">{productName && productName}</div>
+        <div className="category-card__price">{price && price} $</div>
+      </div>
+      <ul className="category-card__colors">
+        {colors &&
+          colors
+            .slice(0, 6)
+            .map((item) => (
+              <li
+                className="category-card__color "
+                key={Math.random()}
+                style={{ backgroundColor: item.hex_value }}
+              ></li>
+            ))}
+      </ul>
+    </div>
+  );
+};
