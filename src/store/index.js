ee0b3fed22ee75546a0676359cb695ff68dc6d23
halikali <tunchalikali@gmail.com>
2022-06-04 15:24:55 +0300

    combine the new reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from "redux";
+import cardReducer from "./reducers/cardReducer";
 import productDetailsReducer from "./reducers/getProductDetails";
 
 import productReducer from "./reducers/productReducer";
@@ -6,6 +7,7 @@ import productReducer from "./reducers/productReducer";
 const rootReducer = combineReducers({
   product: productReducer,
   details: productDetailsReducer,
+  card: cardReducer,
 });
 
 export default rootReducer;
