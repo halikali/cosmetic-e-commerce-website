d56127fed0357f96523224cf03493e0ef252cffc
halikali <tunchalikali@gmail.com>
2022-06-05 21:11:38 +0300

    change url for pagination

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,6 +1,6 @@
-export const getAllProducts = () => {
+export const getAllProducts = (page = 1) => {
   return (dispatch) => {
-    fetch("http://localhost:4000/products/")
+    fetch(`http://localhost:4000/products?_page=${page}&_limit=12`)
       .then((response) => response.json())
       .then((products) =>
         dispatch({ type: "GET_ALL_PRODUCTS_SUCCESS", payload: products })
